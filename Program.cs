f6085bffcb3052d5ba03ea6a01a8c12fe7c43645
MiklFrol <miklfrol@mail.ru>
2022-12-25 23:19:51 +0300

    Задача 23 выполнена

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// Программа принимает n, и выдает таблицу кубов чисел от 1 до n.
+
+Console.Clear();
+
+int GetUserNumber(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+void getCube(int number)
+{
+    for (int i = 1; i < number + 1; i++)
+    {    
+        int temp = i * i * i;
+        if (i < number)
+        {Console.Write($"{temp}, ");}
+        else
+        {Console.Write($"{temp}");}
+    }
+}
+
+int number = GetUserNumber("Введите число:");
+getCube(number);
